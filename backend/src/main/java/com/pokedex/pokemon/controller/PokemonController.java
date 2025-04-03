d837f8b01a5d093f0649812892ac5dd9bd719ffd
= <=>
2025-04-04 00:58:14 +0100

    adding some pokemon attributes and mapping to the controller

--- backend/src/main/java/com/pokedex/pokemon/controller/PokemonController.java
+++ backend/src/main/java/com/pokedex/pokemon/controller/PokemonController.java
@@ -1,8 +1,23 @@
 package com.pokedex.pokemon.controller;
 
-import org.springframework.stereotype.Controller;
+import com.pokedex.pokemon.model.Pokemon;
+import com.pokedex.pokemon.service.PokemonService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/pokemon")
 public class PokemonController {
+    private final PokemonService pokemonService;
 
+    public PokemonController(PokemonService pokemonService) {
+        this.pokemonService = pokemonService;
+    }
+
+    @GetMapping("/{id}")
+    public Pokemon getPokemon(@PathVariable int id) {
+        return null;
+    }
 }
--- backend/src/main/java/com/pokedex/pokemon/service/PokemonService.java
+++ backend/src/main/java/com/pokedex/pokemon/service/PokemonService.java
@@ -1,7 +1,30 @@
 package com.pokedex.pokemon.service;
 
+import com.pokedex.pokemon.model.Pokemon;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PokemonService {
+
+
+
+    private Pokemon pokemonMapper(String response) {
+        JSONObject jsonObject = new JSONObject(response);
+
+        String name;
+        int id;
+        int generationId;
+        String imageUrl;
+        String iconUrl;
+        String type;
+        String species;
+        double height;
+        double weight;
+        List<String> abilities;
+
+        return new Pokemon();
+    }
 }
