906a43befbf9a9496735e071a148712829a1968b
Hugo Faria <altran-hfaria@iefp.pt>
2025-04-04 23:22:52 +0100

    creating find all method in service and controller

---
+++ backend/src/main/java/com/pokedex/pokemon/PokemonAPI.java
@@ -0,0 +1,7 @@
+package com.pokedex.pokemon;
+
+public class PokemonAPI {
+
+    public static final String FIND_ALL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10000";
+    public static final String FIND_BY_NAME = "https://pokeapi.co/api/v2/pokemon/";
+}
--- backend/src/main/java/com/pokedex/pokemon/controller/PokemonController.java
+++ backend/src/main/java/com/pokedex/pokemon/controller/PokemonController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/pokemon")
 public class PokemonController {
@@ -14,6 +17,11 @@ public class PokemonController {
 
     public PokemonController(PokemonService pokemonService) {
         this.pokemonService = pokemonService;
+    }
+
+    @GetMapping
+    public List<Pokemon> getPokemonList() throws IOException, InterruptedException {
+        return pokemonService.findAll();
     }
 
     @GetMapping("/{id}")
